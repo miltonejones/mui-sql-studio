268d8cda8f2d0ab6a724a56573ddd241000b6657
miltonejones <miltonejones@gmail.com>
2022-11-15 03:31:37 -0600

    modal bug

--- src/components/ConnectionModal/ConnectionModal.js
+++ src/components/ConnectionModal/ConnectionModal.js
@@ -66,7 +66,7 @@ export default function ConnectionModal({open, connection = {}, onChange, onClos
     }
   };
 
-  const isValid = () => !!connection && !Object.keys(connection).some(f => !connection[f])
+  const isValid = () =>!0//Object.keys(connection).some(f => !connection[f])
  
   return <Dialog open={open}>
     <DialogTitle>
